180b3495206b9314177e14dfbc971bcdc2405673
Sumitsoni21 <128276751+Sumitsoni21@users.noreply.github.com>
2025-06-03 00:15:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices = document.querySelectorAll(".price");
+	let total = 0;
+
+	prices.forEach(cell=> {
+		total += parseFloat(cell.textContent);
+	})
+
+	const table = document.querySelector("table");
+	const newRow = document.createElement("tr");
+	const newCell = document.createElement("td");
+
+
+	newCell.colSpan = 2; 
+    newCell.textContent = `Total Price: Rs ${total}`;
+    newCell.style.fontWeight = "bold";
+
+    newRow.appendChild(newCell);
+    table.appendChild(newRow);
   
 };
 
